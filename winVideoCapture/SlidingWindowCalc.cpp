dee518e511314e7291c0a44a43db517175dfd7b8
hotkid <poke.poke@126.com>
2016-11-16 23:10:02 +0800

    Add sliding windows .

---
+++ winVideoCapture/SlidingWindowCalc.cpp
@@ -0,0 +1,120 @@
+#include "stdafx.h"
+#include "SlidingWindowCalc.h"
+
+CSlidingWindowCalc::CSlidingWindowCalc()
+	: mInterval(5000)
+	, mTotalSampleCount(0)
+	, mTotalStreamSize(0)
+	, mStartTickCount(GetTickCount())
+{
+
+}
+
+CSlidingWindowCalc::~CSlidingWindowCalc()
+{
+}
+
+void CSlidingWindowCalc::Reset() {
+	mTotalStreamSize = 0;
+	mTotalSampleCount = 0;
+	mStartTickCount = GetTickCount();
+	mSampleList.clear();
+}
+
+void CSlidingWindowCalc::Reset(uint32_t durationInMS) {
+	mInterval = durationInMS;
+	mTotalStreamSize = 0;
+	mTotalSampleCount = 0;
+	mStartTickCount = GetTickCount();
+	mSampleList.clear();
+}
+
+int CSlidingWindowCalc::AppendSample(uint32_t size)
+{
+	mTotalSampleCount++;
+	mTotalStreamSize += size;
+
+	RateSample sample;
+	sample.sampleSize = size;
+	sample.timestamp = GetTickCount();
+	sample.streamSize = mTotalStreamSize;
+	mSampleList.push_back(sample);
+
+	while (Duration() > mInterval)
+		mSampleList.pop_front();
+
+	return 0;
+}
+
+double CSlidingWindowCalc::Frequency() const
+{
+	return (Samples()*1000.0 / Duration());
+}
+
+uint32_t CSlidingWindowCalc::Bitrate() const
+{
+	uint64_t bitrate = SampleSize()*1000 / Duration();
+
+	return (uint32_t)bitrate;
+}
+
+uint32_t CSlidingWindowCalc::MaxSample() const 
+{
+	uint32_t maxSize = 0;
+	for (std::list<RateSample>::const_iterator it = mSampleList.begin();
+		it != mSampleList.end(); ++it) {
+		maxSize = max(maxSize, it->sampleSize);
+	}
+
+	return maxSize;
+}
+
+uint32_t CSlidingWindowCalc::MinSample() const
+{
+	uint32_t minSize = 0xffffffff;
+	for (std::list<RateSample>::const_iterator it = mSampleList.begin();
+		it != mSampleList.end(); ++it) {
+		minSize = min(minSize, it->sampleSize);
+	}
+
+	return minSize;
+}
+
+uint64_t CSlidingWindowCalc::TotalSampleSize() const
+{
+	return mTotalStreamSize;
+}
+
+uint64_t CSlidingWindowCalc::SampleSize() const
+{
+	uint64_t sampleSize = 0;
+	if (mSampleList.size() > 2){
+		sampleSize = mSampleList.back().streamSize - mSampleList.front().streamSize;
+	}
+
+	return sampleSize;
+}
+
+uint64_t CSlidingWindowCalc::TotalSamples() const 
+{
+	return mTotalSampleCount;
+}
+
+uint64_t CSlidingWindowCalc::Samples() const
+{
+	return mSampleList.size();
+}
+
+uint64_t CSlidingWindowCalc::TotalDuration() const
+{
+	return (GetTickCount() - mStartTickCount);
+}
+
+uint64_t CSlidingWindowCalc::Duration() const
+{
+	uint64_t durationTime = 1;
+	if (mSampleList.size() > 2){
+		durationTime = mSampleList.back().timestamp - mSampleList.front().timestamp;
+	}
+	return durationTime;
+}
---
+++ winVideoCapture/SlidingWindowCalc.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <stdint.h>
+#include <Windows.h>
+#include <list>
+
+class CSlidingWindowCalc
+{
+public:
+	CSlidingWindowCalc();
+	~CSlidingWindowCalc();
+
+	void Reset();
+	void Reset(uint32_t durationInMS);
+	uint32_t Bitrate() const;
+	double Frequency() const;
+	int AppendSample(uint32_t size);
+
+	uint32_t MaxSample() const;
+	uint32_t MinSample() const;
+
+	uint64_t TotalSampleSize() const;
+	uint64_t SampleSize() const;
+
+	uint64_t TotalSamples() const;
+	uint64_t Samples() const;
+
+	uint64_t TotalDuration() const;
+	uint64_t Duration() const;
+
+protected:
+	struct RateSample {
+		uint64_t timestamp;
+		uint32_t sampleSize;
+		uint64_t streamSize;
+	};
+
+	uint32_t mInterval;
+	std::list<RateSample> mSampleList;
+	uint64_t mTotalSampleCount;
+	uint64_t mTotalStreamSize;
+	uint64_t mStartTickCount;
+};
+
