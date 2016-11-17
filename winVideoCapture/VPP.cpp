ad0b87fa97fb2c5e78e326f291265d71ea053cc7
hotkid <poke.poke@126.com>
2016-11-17 18:55:00 +0800

    Add vpp project.

---
+++ winVideoCapture/VPP.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "VPP.h"
+
+
+CVPP::CVPP()
+{
+}
+
+
+CVPP::~CVPP()
+{
+}
+
+BOOL CVPP::convertYUY2toYV12()
+{
+	return TRUE;
+}
---
+++ winVideoCapture/VPP.h
@@ -0,0 +1,10 @@
+#pragma once
+class CVPP
+{
+public:
+	CVPP();
+	~CVPP();
+
+	BOOL convertYUY2toYV12();
+};
+
