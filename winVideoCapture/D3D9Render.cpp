261e36056ce00e2df2ccba73b418b008511d2e52
yanli <poke.poke@foxmail.com>
2016-11-25 15:14:47 +0800

    Add d3d9 render.

---
+++ winVideoCapture/D3D9Render.cpp
@@ -0,0 +1,97 @@
+#include "stdafx.h"
+#include "D3D9Render.h"
+
+
+D3D9Render::D3D9Render()
+	: mhWnd(NULL)
+{
+}
+
+D3D9Render::D3D9Render(HWND hWnd)
+	: mhWnd(hWnd)
+{
+
+}
+
+
+D3D9Render::~D3D9Render()
+{
+}
+
+
+DWORD WINAPI RenderThread(LPVOID args)
+{
+	D3D9Render *pRender = (D3D9Render*)args;
+	return pRender->RenderLoop();
+}
+
+HRESULT D3D9Render::InitializeRenderContext(int width, int height, DWORD pixelFormatInFourCC)
+{
+	HRESULT hr = S_OK;
+	D3DPRESENT_PARAMETERS d3dpp; //the presentation parameters that will be used when we will create the device
+	ZeroMemory(&d3dpp, sizeof(d3dpp)); //to be sure d3dpp is empty
+	d3dpp.Windowed = TRUE; //use our global windowed variable to tell if the program is windowed or not
+	d3dpp.hDeviceWindow = mhWnd; //give the window handle of the window we created above
+	d3dpp.BackBufferCount = 1; //set it to only use 1 backbuffer
+	d3dpp.BackBufferWidth = width; //set the buffer to our window width
+	d3dpp.BackBufferHeight = height; //set the buffer to out window height
+	d3dpp.BackBufferFormat = (D3DFORMAT)pixelFormatInFourCC; //the backbuffer format
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD; //SwapEffect
+
+	mPD3D9DOBj = Direct3DCreate9(D3D_SDK_VERSION); //Create the presentation parameters
+
+	CHECK_HR(hr = mPD3D9DOBj->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, mhWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &mPD3DDevice));
+	CHECK_HR(hr = D3DXCreateFont(mPD3DDevice, 15, 0, FW_BOLD, 1, 0, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE, TEXT("Arial"), &mPFont));
+
+done:
+	if (FAILED(hr)){
+		DeinitRenderContext();
+	}
+
+	return hr;
+}
+
+HRESULT D3D9Render::DeinitRenderContext()
+{
+	SAFE_RELEASE(mPFont);
+	SAFE_RELEASE(mPD3DDevice);
+	SAFE_RELEASE(mPD3D9DOBj);
+	return S_OK;
+}
+
+DWORD D3D9Render::RenderLoop()
+{
+	while (mRenderThreadRuning){
+		Sleep(10);
+	}
+
+	return 0;
+}
+
+HRESULT D3D9Render::PushFrame(CSampleBuffer *frame)
+{
+	HRESULT hr = E_FAIL;
+
+	return hr;
+}
+
+BOOL D3D9Render::OSDText(HDC, TCHAR *format, ...)
+{
+	HRESULT hr = S_OK;
+	RECT FontPos;
+	TCHAR buf[1024] = { '\0' };
+	va_list va_alist;
+
+	GetWindowRect(mhWnd, &FontPos);
+
+	va_start(va_alist, format);
+	vswprintf_s(buf, format, va_alist);
+	va_end(va_alist);
+
+	CHECK_HR(hr = mPD3DDevice->BeginScene());
+	hr = mPFont->DrawText(NULL, buf, -1, &FontPos, DT_NOCLIP, D3DCOLOR_ARGB(255, 0, 255, 0));
+	CHECK_HR(hr = mPD3DDevice->EndScene());
+
+done:
+	return hr == S_OK;
+}
---
+++ winVideoCapture/D3D9Render.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <d3d9.h>
+#include <d3dx9.h>
+
+#pragma comment(lib, "d3dx9.lib") //link the library
+#pragma comment(lib, "d3d9.lib") //link the library
+
+class D3D9Render
+{
+public:
+	D3D9Render();
+	D3D9Render(HWND hWnd);
+	~D3D9Render();
+
+	HRESULT InitializeRenderContext(int width, int height, DWORD pixelFormatInFourCC);
+	HRESULT DeinitRenderContext();
+	HRESULT PushFrame(CSampleBuffer *frame);
+	DWORD RenderLoop();
+
+protected:
+	BOOL OSDText(HDC, TCHAR *, ...);
+
+private:
+	HWND mhWnd;
+	IDirect3D9* mPD3D9DOBj;
+	IDirect3DDevice9* mPD3DDevice;
+	LPD3DXFONT mPFont;
+
+
+	HANDLE mRenderEvent;
+	BOOL mSupportVSync;
+	HANDLE mRenderThreadHandle;
+	DWORD mRenderThreadId;
+	BOOL mRenderThreadRuning;
+};
+
