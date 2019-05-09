2afa9bcef33527d42ea5bfb1f1a72efc76abe73a
yanli <yanli@yy.com>
2019-05-09 19:39:24 +0800

    add configure batch files.

---
+++ Build/configure.bat
@@ -0,0 +1,12 @@
+
+echo [info] init debug env
+set ConfigurationName=debug
+xcopy ..\ffmpeg2.7.1\bin\*.dll ..\%ConfigurationName% /y /i /r /D /e 
+xcopy ..\VideoFilters\OrangeFilters\bin\%ConfigurationName%\x86\*.dll ..\%ConfigurationName% /y /i /r /D /e 
+xcopy ..\VideoFilters\effects ..\%ConfigurationName%\effects /y /i /r /D /e 
+
+echo [info] init release env
+set ConfigurationName=release
+xcopy ..\ffmpeg2.7.1\bin\*.dll ..\%ConfigurationName% /y /i /r /D /e 
+xcopy ..\VideoFilters\OrangeFilters\bin\%ConfigurationName%\x86\*.dll ..\%ConfigurationName% /y /i /r /D /e 
+xcopy ..\VideoFilters\effects ..\%ConfigurationName%\effects /y /i /r /D /e 
