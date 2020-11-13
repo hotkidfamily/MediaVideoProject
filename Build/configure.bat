53887dbdc9ca3c8b5c364c1575cb6f797d3d0e9e
yanli <yanli@yy.com>
2020-11-13 17:07:31 +0800

    chore: fix copy script

--- Build/configure.bat
+++ Build/configure.bat
@@ -1,16 +1,15 @@
-set platformTarget=x86
-set ConfigurationName=Debug
-set destDir=..\Output\%platformTarget%\%ConfigurationName%\
 
-echo [info] init %platformTartget% %ConfigurationName% env
+SETLOCAL ENABLEDELAYEDEXPANSION
 
-xcopy ..\3rd\ffmpeg2.7.1\bin\*.dll  %destDir% /y /i /r /D
-xcopy ..\3rd\libx264\libx264-146.dll*   %destDir% /y /i /r /D
+@echo off
 
-echo [info] init release env
-set platformTartget=x86
-set ConfigurationName=Release
-set destDir=..\Output\%platformTarget%\%ConfigurationName%\
+for %%i in (x86) do (
+ for %%j in (Debug Release) do (
+	set destDir=..\Output\%%i\%%j\
 
-xcopy ..\3rd\ffmpeg2.7.1\bin\*.dll  %destDir% /y /i /r /D
-xcopy ..\3rd\libx264\libx264-146.dll*  %destDir% /y /i /r /D
+	echo [info] init %%i %%j env to !destDir!
+
+	xcopy ..\3rd\ffmpeg2.7.1\bin\*.dll  !destDir! /y /i /r /D
+	xcopy ..\3rd\libx264\libx264-146.dll* !destDir! /y /i /r /D
+	)
+)
