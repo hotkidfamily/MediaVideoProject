e81c882a4f00317af79a268915c90888ded75c1a
yanli <yanli@yy.com>
2020-11-13 16:55:09 +0800

    chore: fix copy path

--- Build/configure.bat
+++ Build/configure.bat
@@ -1,13 +1,16 @@
-set platformTartget=x86
-echo [info] init debug env
-set ConfigurationName=debug
-xcopy ..\3rd\ffmpeg2.7.1\bin\*.dll ..\%ConfigurationName% /y /i /r /D /e 
-xcopy ..\3rd\libx264\*.dll ..\%ConfigurationName% /y /i /r /D /e 
+set platformTarget=x86
+set ConfigurationName=Debug
+set destDir=..\Output\%platformTarget%\%ConfigurationName%\
+
+echo [info] init %platformTartget% %ConfigurationName% env
+
+xcopy ..\3rd\ffmpeg2.7.1\bin\*.dll  %destDir% /y /i /r /D
+xcopy ..\3rd\libx264\libx264-146.dll*   %destDir% /y /i /r /D
 
 echo [info] init release env
-set ConfigurationName=release
-xcopy ..\3rd\ffmpeg2.7.1\bin\*.dll ..\%ConfigurationName% /y /i /r /D /e 
-xcopy ..\3rd\libx264\*.dll ..\%ConfigurationName% /y /i /r /D /e 
+set platformTartget=x86
+set ConfigurationName=Release
+set destDir=..\Output\%platformTarget%\%ConfigurationName%\
 
-
-set platformTartget=x64
+xcopy ..\3rd\ffmpeg2.7.1\bin\*.dll  %destDir% /y /i /r /D
+xcopy ..\3rd\libx264\libx264-146.dll*  %destDir% /y /i /r /D
